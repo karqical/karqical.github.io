eb2a55d7dbb0ff640c89e20317b8857c6e4a5987
karqical <under1986@163.com>
2021-07-24 22:24:53 +0800

    update

--- git_push.sh
+++ git_push.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-git config user.name "karqical"
-git config user.email "under1986@163.com"
-#git config credential.helper store
+# git config user.name "karqical"
+# git config user.email "under1986@163.com"
+# git config credential.helper store
 git add -A
 git commit -m 'update'
 git push # https://github.com/karqical/karqical.github.io.git master
